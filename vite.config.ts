f8333081628b4961b70d2f0403c4c550f0a589d5
Jorge von der Brelje <132647789+Jvonderb@users.noreply.github.com>
2026-09-04 09:00:17 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/industrial-operations-dashboard/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
